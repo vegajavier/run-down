966e94753aa533c94b4916f0a8b2c70a8d0035a7
Javier Vega <jvega@Javiers-MBP.attlocal.net>
2019-02-06 19:33:01 -0600

    text

--- src/components/header/About.js
+++ src/components/header/About.js
@@ -14,9 +14,7 @@ export default class About extends Component {
 					<Media.Body>
 						<h5>Javier Vega</h5>
 						<p>
-							Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-							commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
-							condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+							Founder, and sports enthusiast. 
 						</p>
 					</Media.Body>
 				</Media>
